25e8979b65f5841ed2456dc2a8feb911614d74f9
Daya <ariannevalencia@gmail.com>
2018-10-31 18:04:58 +0800

    hardcoded questions that were in the document HAHA

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from  kivy.uix.filechooser import FileChooserIconView
+from kivy.uix.filechooser import FileChooserIconView
 
 class ScreenOne(Screen):
     pass
@@ -15,10 +15,35 @@ class ScreenOne(Screen):
 class ScreenTwo(Screen):
     pass
 
+# quiz -------------------------------
 class ScreenThree(Screen):
     pass
 
 class Question2(Screen):
+    pass
+
+class Question3(Screen):
+    pass
+
+class Question4(Screen):
+    pass
+
+class Question5(Screen):
+    pass
+
+class Question6(Screen):
+    pass
+
+class Question7(Screen):
+    pass
+
+class Question8(Screen):
+    pass
+
+class Question9(Screen):
+    pass
+
+class Question10(Screen):
     pass
 
 class WYPupper(App):
@@ -28,8 +53,17 @@ class WYPupper(App):
 
         screen_manager.add_widget(ScreenOne(name = "screen_one")) #home
         screen_manager.add_widget(ScreenTwo(name = "screen_two")) #filechooser
+
         screen_manager.add_widget(ScreenThree(name = "screen_three")) #quizstart
         screen_manager.add_widget(Question2(name = "question_2")) #q2
+        screen_manager.add_widget(Question3(name = "question_3")) #q3
+        screen_manager.add_widget(Question4(name = "question_4")) #q4
+        screen_manager.add_widget(Question5(name = "question_5")) #q5
+        screen_manager.add_widget(Question6(name = "question_6")) #q6
+        screen_manager.add_widget(Question7(name = "question_7")) #q7
+        screen_manager.add_widget(Question8(name = "question_8")) #q8
+        screen_manager.add_widget(Question9(name = "question_9")) #q9
+        screen_manager.add_widget(Question10(name = "question_10")) #q10
 
         return screen_manager
 
